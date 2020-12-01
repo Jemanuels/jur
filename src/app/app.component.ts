d59278d79403a9f257885be696b69240e1d9e4de
Jurgen Emanuels <Jurgen.Emanuels@CyberIAM.com>
2020-12-01 08:56:11 +0200

    Key Features of Angular Core

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jur';
+
+  data = {title: 'Jurgen'};
+
+  onKeyUp(newTitle: string){
+    this.data.title = newTitle;
+  }
 }
